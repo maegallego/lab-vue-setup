061d89c8cb91b9329a3ee0fd6736d8e1316d2581
maegallego <107775408+maegallego@users.noreply.github.com>
2022-08-16 21:13:52 +0200

    Iterations 1-3

---
+++ HelloButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="HelloWorld">Pasar saludo por consola. </button>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    data: String,
+    msg: String,
+  },
+  methods: {
+    HelloWorld() {
+      console.log('Hello world!');
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDoList></ToDoList>
+    <HelloButton></HelloButton>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import ToDoList from '@/components/ToDoList.vue';
+import HelloButton from '@/components/HelloButton.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    ToDoList,
+    HelloButton,
+  },
+};
+</script>
